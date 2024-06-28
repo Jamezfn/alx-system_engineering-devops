32b017771ff5db7c3b5cb3dbafee6a2415c6a78a
James Mukhola <jamesmakachia19@gmail.com>
2024-06-28 23:22:08 +0300

    configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,8 @@
 #!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
-package {'python::pip':
+
+package {'flask':
   name => 'flask',
-  pkgname => 'flask'
   ensure   => '2.1.0',
-  pip_provider => 'pip3'
+  provider => 'pip3'
 }
