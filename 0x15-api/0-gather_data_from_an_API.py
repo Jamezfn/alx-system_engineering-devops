235c57bc7d926e96f531b52f2f17d7fd4f1ed6a8
Jamezfn <jamesmakachia19@gmail.com>
2024-07-29 05:44:22 -0700

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Fetch and display TODO list for an employee ID"""
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
 
@@ -9,7 +9,6 @@ if __name__ == "__main__":
     todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
     completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{})".format(
+    print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todos)))
-    for c in completed:
-        print("\t {}".format(c))
+    [print("\t {}".format(c)) for c in completed]
