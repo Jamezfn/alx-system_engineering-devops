1e48be748945dc9ff10eda0100f60cef962b5518
Jamezfn <jamesmakachia19@gmail.com>
2025-06-30 02:51:50 -0700

    Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
-# install flask from pip3.
-
-package { 'flask':
-    ensure   => '2.1.0',
-    name     => 'flask',
-    provider => 'pip3',
+exec {'install_flask_with_werkzeug':
+  command => '/usr/bin/pip3 install flask==2.1.0 && /usr/bin/pip3 install werkzeug==2.1.1',
+  path => ['/usr/bin', '/usr/bin'],
+  unless => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
 }
